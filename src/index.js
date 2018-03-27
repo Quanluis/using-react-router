e465f3a7b78bc4d3cb4968f259868cb4daad957e
quan <quanluis@protonmail.com>
2018-03-27 18:44:46 -0400

    set up redux store and connected the provider to the index.js also created store component

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './Store';
 
 ReactDOM.render(
+    <Provider store = {store}>
     <Router>
        <App />
-    </Router >, document.getElementById('root'));
+    </Router >
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
